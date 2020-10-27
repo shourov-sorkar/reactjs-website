d619d73b820fad17f3d33eb7ee2123f5d0dfbee7
Md Monir Hossain Showrav <18103142@iubat.edu>
2020-10-28 02:37:12 +0600

    add responsive carousel

--- src/About.jsx
+++ src/About.jsx
@@ -1,9 +1,47 @@
 import React from "react";
+import Carousel from "../node_modules/react-bootstrap/Carousel";
+import slide from "../src/images/slide1.jpg";
 
 const About = () => {
     return (
         <>
-        <h1>Welcome About Page</h1>
+        <div className="container-fluid-xl">
+        <Carousel className>
+  <Carousel.Item interval={1000}>
+    <img
+      className="d-block w-100"
+      src={slide}
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item interval={500}>
+    <img
+      className="d-block w-100"
+      src={slide}
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={slide}
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+</div>
         </>
     );
 };
