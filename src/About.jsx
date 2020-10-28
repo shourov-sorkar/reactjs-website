ec2bcba2df24dcbf0acb1245d497740b025e9cae
Md Monir Hossain Showrav <18103142@iubat.edu>
2020-10-28 22:09:20 +0600

    :heavy_plus_sign:add services section

--- src/About.jsx
+++ src/About.jsx
@@ -5,43 +5,43 @@ import slide from "../src/images/slide1.jpg";
 const About = () => {
     return (
         <>
-        <div className="container-fluid-xl">
-        <Carousel className>
-  <Carousel.Item interval={1000}>
-    <img
-      className="d-block w-100"
-      src={slide}
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item interval={500}>
-    <img
-      className="d-block w-100"
-      src={slide}
-      alt="Third slide"
-    />
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src={slide}
-      alt="Third slide"
-    />
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-</div>
+            <div className="container-fluid-xl">
+                <Carousel className>
+                    <Carousel.Item interval={1000}>
+                        <img
+                            className="d-block w-100"
+                            src={slide}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item interval={500}>
+                        <img
+                            className="d-block w-100"
+                            src={slide}
+                            alt="Third slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={slide}
+                            alt="Third slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
         </>
     );
 };
---
+++ src/Sdata.jsx
@@ -0,0 +1,41 @@
+import web from "../src/images/web.jpg";
+import app from "../src/images/app.png";
+import banking from "../src/images/banking.jpg";
+import digital from "../src/images/digital.png";
+import graphics from "../src/images/graphics.jpg";
+import ai from "../src/images/ai.jpg";
+
+const Sdata  = [
+    {
+        imgsrc: web,
+        title: "Web Development",
+        atext:"We will be create your web application from scratch. If you have idea about what you need to do, share us.",
+    },
+    {
+        imgsrc: app,
+        title: "Mobile Apps Development",
+        atext: "Developing innovative and native mobile Apps for Android and iOS platforms.",
+    },
+    {
+        imgsrc: banking,
+        title: "Banking Solution",
+        atext:"Customized software solutions tailored for your unique banking & financial business needs.",
+    },
+    {
+        imgsrc: digital,
+        title: "E-Commerce Solution",
+        atext: "Focusing on creating user-centric experiences and meaningful identities.",
+    },
+    {
+        imgsrc: graphics,
+        title: "Graphics Design Solution",
+        atext: " Provide ultimate e-commerce solution as well as customizations for your e-commerce business",
+    },
+    {
+        imgsrc: ai,
+        title: "AI/Ml Solution",
+        atext:"Machine Learning and Artificial Intelligence solution for your organization by experts hand.",
+    },
+];
+
+export default Sdata;
